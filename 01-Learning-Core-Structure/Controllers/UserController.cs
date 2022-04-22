c5d99fb89fd87f96b0ab8618592ba8fff27ea4fa
Vinicius pereira de Oliveira <act061@einstein.br>
2022-04-22 19:07:50 -0300

    01- Learning Core Structure

---
+++ 01-Learning-Core-Structure/Controllers/UserController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace _01_Learning_Core_Structure.Controllers {
+    [ApiController]
+    [Route("/user")]
+    public class UserController : BaseController {
+        
+    }
+}
